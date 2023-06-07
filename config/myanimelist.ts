631efe16f5d3b0ae2583f87cb03c0405ba149287
Олександр Козак <kozackunisoft@gmail.com>
2023-06-07 14:51:50 +0300

    fix: env

--- config/myanimelist.ts
+++ config/myanimelist.ts
@@ -2,5 +2,5 @@ import { load } from "https://deno.land/std/dotenv/mod.ts";
 
 const env = await load();
 
-export const CLIENT_ID = env['MAL_CLIENT_ID']
-export const USER_NAME = env['MAL_USER_NAME']
+export const CLIENT_ID = env['MAL_CLIENT_ID'] || Deno.env.get('MAL_CLIENT_ID')
+export const USER_NAME = env['MAL_USER_NAME'] || Deno.env.get('MAL_USER_NAME')
--- config/telegram.ts
+++ config/telegram.ts
@@ -2,5 +2,5 @@ import { load } from "https://deno.land/std/dotenv/mod.ts";
 
 const env = await load();
 
-export const BOT_KEY = env['BOT_KEY']
-export const CHAT_ID = env['CHAT_ID']
+export const BOT_KEY = env['BOT_KEY'] || Deno.env.get('BOT_KEY')
+export const CHAT_ID = env['CHAT_ID'] || Deno.env.get('CHAT_ID')
